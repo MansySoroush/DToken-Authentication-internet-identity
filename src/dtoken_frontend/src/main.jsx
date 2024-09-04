0e94c55f9555250e1163ea3c172232b9d2fb9b71
MansySoroush <mansy.soroush@gmail.com>
2024-09-04 15:31:37 -0400

    Successful Commit: DTocken & Authenticating with Internet Identity

--- src/dtoken_frontend/src/main.jsx
+++ src/dtoken_frontend/src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from "./components/App";
 import './index.scss';
+import { AuthClient } from "@dfinity/auth-client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const authClient = await AuthClient.create();
+
+if (await authClient.isAuthenticated()) {
+  handleAuthenticated(authClient);
+} else {
+  await authClient.login({
+    identityProvider: "https://identity.ic0.app/#authorize",
+    onSuccess: () => {
+      handleAuthenticated(authClient);
+    },
+  });
+}
+
+async function handleAuthenticated(authClient) {
+  const identity = await authClient.getIdentity();
+  const userPrincipal = identity._principal.toString();
+  console.log(userPrincipal);
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App loggedInPrincipal={userPrincipal} />
+    </React.StrictMode>,
+  );
+}
